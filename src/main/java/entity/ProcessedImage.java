b07542e3c4b664afe6de5a89345993d88a01a545
bmodahl <bmodahl87@gmail.com>
2017-03-22 16:55:29 -0500

    Added ImageProcessing entity

---
+++ src/main/java/entity/ProcessedImage.java
@@ -0,0 +1,33 @@
+package entity;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by bmodahl on 3/21/17.
+ */
+
+@XmlRootElement(name = "processed_image")
+public class ProcessedImage {
+
+
+    Boolean success;
+    byte[] image;
+
+    public ProcessedImage() {};
+
+
+    public Boolean getSucess() { return success; }
+
+    public void setSuccess(Boolean success) { this.success = success; }
+
+
+    public byte[] getImage() { return image; }
+
+    public void setImage(byte[] image) { this.image = image; }
+
+
+
+
+}
