dcf4c5e43d64b54853ed0af070db9b5e380adda5
cgreger <greger@madisoncollege.edu>
2017-03-28 13:50:03 -0500

    Updating pom to handle javax servlet

--- src/main/java/controller/TemporaryImageServlet.java
+++ src/main/java/controller/TemporaryImageServlet.java
@@ -1,9 +1,21 @@
 package controller;
 
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
 /**
  * Created by katana on 3/28/17.
  */
-public class TemporaryImageServlet {
+@WebServlet(
+
+        name = "TemporaryImageServlet",
+        urlPatterns = {"/" + generatedPath}
+
+
+)
+
+public class TemporaryImageServlet extends HttpServlet {
+
 
 
 }
