b3db8691ec9eeaa602248412fbe5a3cdebd43325
cgreger <greger@madisoncollege.edu>
2017-04-10 21:51:01 -0500

    adding logger

--- src/test/java/resizer/ImageResizeTest.java
+++ src/test/java/resizer/ImageResizeTest.java
@@ -1,10 +1,12 @@
 package resizer;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -14,7 +16,7 @@ import static org.junit.Assert.*;
  */
 public class ImageResizeTest {
 
-    private Logger log =
+    private Logger log = Logger.getLogger(this.getClass());
     private ImageResize resizer;
     private URL pic1Url;
     private URL pic2Url;
@@ -24,9 +26,11 @@ public class ImageResizeTest {
     @Before
     public void setUp() throws Exception {
 
+        urls = new ArrayList<URL>();
+
         resizer = new ImageResize();
-        URL pic1Url = new URL("http://www.clipartbest.com/cliparts/KTn/XXK/KTnXXK8Ec.png");
-        URL pic2Url = new URL("http://www.iconsdb.com/icons/preview/orange/fish-xxl.png");
+        pic1Url = new URL("http://www.clipartbest.com/cliparts/KTn/XXK/KTnXXK8Ec.png");
+        pic2Url = new URL("http://www.iconsdb.com/icons/preview/orange/fish-xxl.png");
 
         urls.add(pic1Url);
         urls.add(pic2Url);
@@ -38,7 +42,7 @@ public class ImageResizeTest {
     public void resizeImage() throws Exception {
 
         response = resizer.resizeImage(urls, 300, 100);
-        log.info(response.toString());
+        log.info("RESPONSE -----\n\n" + response.toString());
 
     }
 
