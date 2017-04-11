e24a3027ab8822692c5d230a4e09a5055a07df19
Keith <khlarson@madisoncollege.edu>
2017-04-11 14:15:40 -0500

    bla

--- src/main/java/resizer/ImageResize.java
+++ src/main/java/resizer/ImageResize.java
@@ -27,7 +27,7 @@ public class ImageResize {
 
     ProcessedImage processedImage = new ProcessedImage();
 
-    @Path("/resizeImageJpeg")
+    @Path("/resizeImage")
     @GET
     public Response resizeImage(@QueryParam("urls") List<URL> urls,
                                 @QueryParam("width") int width,
@@ -37,7 +37,7 @@ public class ImageResize {
 
     }
 
-    @Path("/resizeImageJpeg")
+    @Path("/resizeImage")
     @GET
     public Response resizeImage(@QueryParam("urls") List<URL> urls,
                                 @QueryParam("width") int width,
@@ -48,14 +48,14 @@ public class ImageResize {
 
     }
 
-    @Path("/resizeImageJpeg")
+    @Path("/resizeImage")
     @GET
     public Response resizeImageWidthOnly(@QueryParam("urls") List<URL> urls,
                                          @QueryParam("width") int width) throws IOException {
         return processRequest(urls, 0, width, 1);
 
     }
-    @Path("/resizeImageJpeg")
+    @Path("/resizeImage")
     @GET
     public Response resizeImageWidthOnly(@QueryParam("urls") List<URL> urls,
                                          @QueryParam("width") int width,
@@ -64,7 +64,7 @@ public class ImageResize {
 
     }
 
-    @Path("/resizeImageJpeg")
+    @Path("/resizeImage")
     @GET
     public Response resizeImageHeightOnly(@QueryParam("urls") List<URL> urls,
                                           @QueryParam("height") int height) throws IOException {
@@ -72,7 +72,7 @@ public class ImageResize {
         return processRequest(urls, height, 0, 1);
 
     }
-    @Path("/resizeImageJpeg")
+    @Path("/resizeImage")
     @GET
     public Response resizeImageHeightOnly(@QueryParam("urls") List<URL> urls,
                                           @QueryParam("height") int height,
@@ -121,11 +121,12 @@ public class ImageResize {
 
 
             boolean isImage = contentType.startsWith("image/");
-            if(isImage == false){
+            if(isImage == false) {
 
                 processedImage.setSuccess(false);
                 processedImage.setMessage("Invalid URL!");
                 return false;
+            }
         }
         return true;
     }
@@ -173,7 +174,6 @@ public class ImageResize {
         }
 
         return imageData;
-
     }
 
     public List<BufferedImage> resizeImages(List<URL> urls, int width, int height) throws IOException {
@@ -235,7 +235,6 @@ public class ImageResize {
         for (URL url : urls) {
             images.add(ImageIO.read(url));
         }
-
         return images;
     }
 
