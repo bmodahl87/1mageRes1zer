29764971037240d9f8744b5392891d345012a3af
cgreger <greger@madisoncollege.edu>
2017-04-12 22:13:45 -0500

    Updating comments and logging in GifGenerator

--- src/main/java/resizer/GIFGenerator.java
+++ src/main/java/resizer/GIFGenerator.java
@@ -1,38 +1,44 @@
-package resizer;
+/**
+ * GIFGenerator
+ *
+ * Class used to generate
+ * gifs as a ByteArrayOutputStream
+ *
+ */
 
+package resizer;
 import com.madgag.gif.fmsware.AnimatedGifEncoder;
 import org.apache.log4j.Logger;
-
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
-/**
- * Created by katana on 3/27/17.
- */
+
 public class GIFGenerator {
 
-    private AnimatedGifEncoder encoder;
     private Logger log = Logger.getLogger(this.getClass());
+    private AnimatedGifEncoder encoder;
 
     public GIFGenerator() {
 
+        log.info("New gif generator created");
         encoder = new AnimatedGifEncoder();
 
     }
 
     public ByteArrayOutputStream generate(double delayInSeconds, List<BufferedImage> images) {
 
+        log.info("Start gif generation");
         //Create a ByteArrayOutputStream to write the image to
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ByteArrayOutputStream gifStream = new ByteArrayOutputStream();
 
         //Takes in OutputStream
-        encoder.start(outputStream);
+        encoder.start(gifStream);
 
         //Set delay (converted to milliseconds)
         encoder.setDelay((int) Math.round(1000 * delayInSeconds));
 
-        log.info("DELAY within Generator: " + delayInSeconds);
+        log.debug("Delay within generator: " + delayInSeconds);
 
         //Set the gif to repeat (0 = infinite, -1 = 1 cycle, >= 1 indicates number of cycles)
         encoder.setRepeat(0);
@@ -40,6 +46,7 @@ public class GIFGenerator {
         //Add each image to the gif
         for (BufferedImage frame : images) {
 
+            log.debug("Adding frame to gif");
             encoder.addFrame(frame);
 
         }
@@ -47,10 +54,10 @@ public class GIFGenerator {
         //Finalizes gif
         encoder.finish();
 
-        return outputStream;
+        log.info("Finished gif generation");
+
+        return gifStream;
 
     }
-
-
 
 }
